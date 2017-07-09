4674753040076cd205f36bb640961b7c366c0ed9
Andry695 <andrea.cocci@live.it>
2017-07-09 18:36:56 +0200

    Modificato menu laterale

--- ClientSide/src/app/app.component.ts
+++ ClientSide/src/app/app.component.ts
@@ -4,14 +4,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { SettingsPage} from '../pages/settings/settings'
-import { AvatarPage} from '../pages/avatar/avatar'
+import { SettingsPage} from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 import { TassePage } from '../pages/tasse/tasse';
 import { LibrettoPage } from '../pages/libretto/libretto';
 import { PianodistudiPage } from '../pages/pianodistudi/pianodistudi';
-import { NomemateriaPage } from '../pages/nomemateria/nomemateria';
 
 @Component({
   templateUrl: 'app.html'
@@ -21,21 +18,19 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{icon: string, title: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'Settings', component: SettingsPage },
-      { title: 'AvatarPage', component: AvatarPage },
-      { title: 'Orario', component: TabsPage },
-      { title: 'Tasse', component: TassePage },
-      { title: 'Libretto', component: LibrettoPage },
-      { title: 'Piano di studi', component: PianodistudiPage }
+      { icon: 'home', title: 'Home', component: HomePage },
+      { icon: 'time', title: 'Orario', component: TabsPage },
+      { icon: 'cash', title: 'Tasse', component: TassePage },
+      { icon: 'book', title: 'Libretto', component: LibrettoPage },
+      { icon: 'school', title: 'Piano di studi', component: PianodistudiPage },
+      { icon: 'settings', title: 'Impostazioni', component: SettingsPage }
     ];
 
   }
