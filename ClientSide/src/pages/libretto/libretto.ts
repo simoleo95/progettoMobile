994267f6ffd3a4bd4f6a31d2b9a9ceaba82dd308
Andry695 <andrea.cocci@live.it>
2017-11-05 17:46:53 +0100

    Inizializzazione calcolo media in libretto

--- ClientSide/src/pages/libretto/libretto.ts
+++ ClientSide/src/pages/libretto/libretto.ts
@@ -33,39 +33,12 @@ export class LibrettoPage {
 			   public JsonService: JsonDataProvider, private auth: AuthServiceProvider) {
 	  
       this.JsonService.getData(this.auth.getUserInfo()).then(data => {
-          
-        for (let entry in data['libretto']) {
-            this.esami.push(data['libretto'][entry]);
-        }
-//		  let crediti: number = 0;
-//		  let votoTotale: number = 0;
-//		  let numeratorePonderata: number = 0;
-//		  let numeroMaterie: number = 0;
-//           for (let esame in data['libretto']) {
-//			   let voto: string;
-//			   if(esame['materia']['tipocfu'] === 'F') {
-//				   voto = 'Superato';
-//			   } else {
-//				   numeroMaterie++;
-//				   crediti += esame['materia']['cfu'];
-//				   if (esame['voto'] > 30) {
-//					   voto = '30L';
-//					   votoTotale += 30;
-//				   } else {
-//						votoTotale += esame['voto'];
-//				   }
-//				   numeratorePonderata += parseInt(voto, 10) * esame['materia']['cfu'];
-//			   }
-//			   this.libretto.esami.push({
-//				  nomeMateria: esame['materia']['nome'],
-//				   data: esame['appello']['data'],
-//				   voto: voto,
-//				   cfu: esame['materia']['cfu']
-//			   });
-//           }
-//		  this.libretto.mediaAritmetica = votoTotale / numeroMaterie;
-//		  this.libretto.mediaPonderata = numeratorePonderata / crediti;
-       });
+            for(let entry in data['libretto'])
+                this.esami.push(data['libretto'][entry]);
+      });
+        
+      this.calcolaMedia();
+       
   }
 
   ionViewDidLoad() {
@@ -74,6 +47,10 @@ export class LibrettoPage {
     
   openAvatar() {
       this.appCtrl.getRootNav().push(AvatarPage);
+  }
+      
+  calcolaMedia() {
+    // Da completare
   }
 
 }
